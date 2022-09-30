b78dbc1279f2c63314afe74f1b0e1a8ca309c672
lahiruchalana <lahiruchalanairesh@gmail.com>
2022-09-30 23:58:23 +0530

    [currency-rate-controller] completed put and delete methods in currency rates

--- src/main/java/com/myeconomy/worldeconomyindex/controller/CurrencyRateController.java
+++ src/main/java/com/myeconomy/worldeconomyindex/controller/CurrencyRateController.java
@@ -2,6 +2,7 @@ package com.myeconomy.worldeconomyindex.controller;
 
 import com.myeconomy.worldeconomyindex.model.CurrencyRate;
 import com.myeconomy.worldeconomyindex.service.CurrencyRateService;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -23,15 +24,55 @@ public class CurrencyRateController {
     }
 
     @PostMapping
-    public ResponseEntity<?> addNewCurrencyRateData(
+    public ResponseEntity<?> addNewCurrencyRate(
             @RequestBody CurrencyRate currencyRate
     ) {
-        currencyRateService.addNewCurrencyRateData(currencyRate);
+        currencyRateService.addNewCurrencyRate(currencyRate);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @GetMapping
-    public ResponseEntity<List<CurrencyRate>> getAllCurrencyRateData() {
-        return new ResponseEntity<>(currencyRateService.getAllCurrencyRateData(), HttpStatus.OK);
+    public ResponseEntity<List<CurrencyRate>> getCurrencyRates() {
+        return new ResponseEntity<>(currencyRateService.getCurrencyRates(), HttpStatus.OK);
+    }
+
+    @PutMapping(path = "currencies/{currencyId}/currencyRates/{currencyRateId}")
+    public ResponseEntity<?> updateCurrencyRate(
+            @PathVariable("currencyRateId") Long currencyRateId,
+            @PathVariable("currencyId") Long currencyId,
+            @RequestBody CurrencyRate currencyRate
+    ) {
+        currencyRateService.updateCurrencyRate(currencyRateId, currencyId, currencyRate);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @GetMapping(path = "currencies/{currencyName}/allCurrencyRates")
+    public ResponseEntity<List<CurrencyRate>> getCurrencyRatesByCurrencyAndEqualsCurrency(
+            @PathVariable("currencyName") String currencyName,
+            @RequestParam(required = false) String equalsCurrencyName,
+            @RequestParam(required = false) String sortingProperty,
+            @RequestParam(required = false) String order
+    ) {
+        return new ResponseEntity<>(currencyRateService.getCurrencyRatesByCurrencyAndEqualsCurrency(currencyName, equalsCurrencyName, sortingProperty, order), HttpStatus.OK);
+    }
+
+    @DeleteMapping(path = "currencyRates/{currencyRateId}")
+    public ResponseEntity<?> deleteCurrencyRate(
+            @PathVariable("currencyRateId") Long currencyRateId
+    ) {
+        currencyRateService.deleteCurrencyRate(currencyRateId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @GetMapping(path = "currencies/{currencyName}/paginationAllCurrencyRates")
+    public ResponseEntity<Page<CurrencyRate>> getCurrencyRatesWithPagination(
+            @PathVariable("currencyName") String currencyName,
+            @RequestParam(required = false) String equalsCurrencyName,
+            @RequestParam(required = false) Integer pageNumber,
+            @RequestParam(required = false) Integer pageSize,
+            @RequestParam(required = false) String sortingProperty,
+            @RequestParam(required = false) String order
+    ) {
+        return new ResponseEntity<>(currencyRateService.getCurrencyRatesWithPagination(currencyName, equalsCurrencyName, pageNumber,pageSize ,sortingProperty, order), HttpStatus.OK);
     }
 }
