d0a72d7e2bc6086db17442d16641e6f5d24606fb
lahiruchalana <lahiruchalanairesh@gmail.com>
2022-10-02 09:14:40 +0530

    [gdp-controller] intergrated get by country id and country name, put and delete methods

--- src/main/java/com/myeconomy/worldeconomyindex/repository/GdpRepository.java
+++ src/main/java/com/myeconomy/worldeconomyindex/repository/GdpRepository.java
@@ -2,6 +2,7 @@ package com.myeconomy.worldeconomyindex.repository;
 
 import com.myeconomy.worldeconomyindex.model.Gdp;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,4 +11,7 @@ import java.util.List;
 public interface GdpRepository extends JpaRepository<Gdp, Long> {
 
     List<Gdp> findGdpsByCountryCountryName(String countryName);
+
+    @Query("select s from Gdp s where s.country.countryId = ?1")
+    List<Gdp> findGdpsByCountryId(Long countryId);
 }
